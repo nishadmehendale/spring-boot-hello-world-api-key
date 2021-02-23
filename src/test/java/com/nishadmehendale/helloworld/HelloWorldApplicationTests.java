be2a25ad48f6dd19a02a2d859ab73726710a5360
Nishad Mehendale <26198718+nishadmehendale@users.noreply.github.com>
2021-02-23 12:12:01 +0530

    hello-world commit

---
+++ src/test/java/com/nishadmehendale/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishadmehendale.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
